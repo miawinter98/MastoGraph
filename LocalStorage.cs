18ccd193ca7e3aec57c59c85cae1f74622f14a7b
Mia Winter <mia_winter98@hotmail.com>
2022-11-27 10:58:36 +0100

    Commit everything by command line since Visual Studio can't be trusted

---
+++ LocalStorage.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.JSInterop;
+
+namespace MastoGraph; 
+
+public class LocalStorage : IAsyncDisposable {
+	private Lazy<IJSObjectReference> _accessorJsRef = new();
+	private readonly IJSRuntime _jsRuntime;
+
+	public LocalStorage(IJSRuntime jsRuntime) {
+		_jsRuntime = jsRuntime;
+	}
+
+	private async Task WaitForReference() {
+		if (_accessorJsRef.IsValueCreated is false) {
+			_accessorJsRef = new Lazy<IJSObjectReference>(
+				await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "/js/LocalStorage.js"));
+		}
+	}
+
+	public async ValueTask DisposeAsync() {
+		if (_accessorJsRef.IsValueCreated) {
+			await _accessorJsRef.Value.DisposeAsync();
+		}
+		GC.SuppressFinalize(this);
+	}
+
+	// ------------------------------------------------------
+	
+	public async Task ClearAsync() {
+		await WaitForReference();
+		await _accessorJsRef.Value.InvokeVoidAsync("clear");
+	}
+	
+	public async Task RemoveValueAsync(string key) {
+		await WaitForReference();
+		await _accessorJsRef.Value.InvokeVoidAsync("remove", key);
+	}
+	
+	public async Task<T> GetValueAsync<T>(string key) {
+		await WaitForReference();
+		return await _accessorJsRef.Value.InvokeAsync<T>("get", key);
+	}
+
+	public async Task SetValueAsync<T>(string key, T value) {
+		await WaitForReference();
+		await _accessorJsRef.Value.InvokeVoidAsync("set", key, value);
+	}
+}
---
+++ StatusTreeNode.cs
@@ -0,0 +1,23 @@
+﻿using Mastonet.Entities;
+
+namespace MastoGraph; 
+
+public class StatusTreeNode {
+	public Status Status { get; }
+	public bool IsRoot { get; init; } = false;
+
+	public List<StatusTreeNode> Children { get; } = new();
+	
+	public StatusTreeNode(Status status) {
+		Status = status;
+	}
+
+	public bool AddNode(StatusTreeNode node) {
+		if (Status.Id == node.Status.InReplyToId) {
+			Children.Add(node);
+			return true;
+		}
+
+		return Children.Any(child => child.AddNode(node));
+	}
+}
