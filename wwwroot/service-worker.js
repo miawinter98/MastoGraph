3889a3e66b49664f9dae6e318517792b54bef497
Mia Winter <mia_winter98@hotmail.com>
2022-12-11 15:28:52 +0100

    implemented status attachment (including sensitive)

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -1,7 +1,7 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { 
+self.addEventListener('fetch', (event) => { 
     if (event.request.method === 'POST' && url.pathname === '/') {
         event.respondWith((async () => {
             const formData = await event.request.formData();
